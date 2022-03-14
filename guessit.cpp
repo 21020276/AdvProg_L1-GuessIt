d482e7fd0651d5f521db7f73b1694a891955cc4d
21020276 <100508571+21020276@users.noreply.github.com>
2022-03-14 14:23:41 +0700

    Update guessit.cpp

--- guessit.cpp
+++ guessit.cpp
@@ -129,13 +129,4 @@ void printAnswer(int guess, int secretNumber){
  	}
 }
 
-int main(){
-	int secretNumber = generateRandomNumber();
- 	int guess;
- 	
- 	do {
- 		guess = getPlayerGuess();
- 		printAnswer(guess, secretNumber);
- 	} while (guess != secretNumber);
-	
-} 
+
